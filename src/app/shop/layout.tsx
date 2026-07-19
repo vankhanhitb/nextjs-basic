e180681085bf03ea54a551caf6a06fb612f4fe52
Joseph <vankhanhkt12b@gmail.com>
2026-07-19 09:21:36 +0700

    correct root folder

--- src/app/shop/layout.tsx
+++ src/app/shop/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {ErrorWrapper} from './error-wrapper';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import {ErrorWrapper} from '../error-wrapper';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
