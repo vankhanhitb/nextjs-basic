d76244f29ebd8616cc9248bc59ed536020ee7b31
Joseph <vankhanhkt12b@gmail.com>
2026-07-20 10:11:34 +0700

    correct group router structure

---
+++ src/app/(client)/layout.tsx
@@ -0,0 +1,10 @@
+
+export default function LayoutAccount({
+  children
+}:Readonly<{children: React.ReactNode;}>) {
+  return (
+  <div>
+    {children}
+  </div>
+  );
+}
---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function LayoutShop({
+  children
+}:Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
